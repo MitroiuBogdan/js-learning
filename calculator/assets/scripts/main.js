dd865d2076e6bd91f97389b9677bdf11468752e1
bogdan.mitroiu <mitroiu.bogdan@gmail.com>
2022-06-26 16:24:14 +0300

    calculator

---
+++ calculator/assets/scripts/main.js
@@ -0,0 +1,11 @@
+let currentResult = 10;
+
+currentResult = currentResult + 10;
+
+let calculationDescription='(0 + 10)'
+let errorMessage = 'An error occured \n' +
+                   'An error occured'
+
+add(currentResult,120)
+
+addBtn.addEventListener('click',add)
